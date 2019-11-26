7e773e033de5b42b6261542bc525b5056ad7ce30
Adam Reback <adamreback@Adams-MBP.attlocal.net>
2019-11-26 11:58:25 -0800

    Client | Now serves bundled static files for home page

--- server/index.js
+++ server/index.js
@@ -9,6 +9,6 @@ app.use(morgan('dev'));
 app.use(parser.json());
 app.use(parser.urlencoded({extended: true}));
 
-app.use('/', express.static(path.join(__dirname, '../client/dist')));
+app.use('/', express.static(path.join(__dirname, '../client/homePage/dist')));
 
 app.listen(port, () => console.log(`Now listening on port ${port}`));
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 var path = require('path');
 var home_DIR = path.join(__dirname, '/client/homePage/src');
-var admin_DIR = path.join(__dirname, '/client/adminPage/src');
+// var admin_DIR = path.join(__dirname, '/client/adminPage/src');
 var home_DIST_DIR = path.join(__dirname, '/client/homePage/dist');
-var admin_DIST_DIR = path.join(__dirname, 'client/adminPage/dist')
+// var admin_DIST_DIR = path.join(__dirname, 'client/adminPage/dist');
 
 module.exports = [
   //home config
@@ -40,37 +40,37 @@ module.exports = [
     }
   },
   //admin config
-  {
-    entry: {
-      homePage: `${admin_DIR}/index.jsx`
-    },
-    output: {
-      filename: 'bundle.js',
-      path: admin_DIST_DIR
-    },
-    module: {
-      rules: [{
-          test: /\.js[x]?/,
-          include: [admin_DIR],
-          loader: 'babel-loader',
-          query: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        },
-        {
-          test: /\.css$/,
-          use: [{
-              loader: "style-loader" // creates style nodes from JS strings
-            },
-            {
-              loader: "css-loader" // translates CSS into CommonJS
-            }
-          ]
-        }
-      ]
-    },
-    resolve: {
-      extensions: ['.js', '.jsx']
-    }
-  }
+  // {
+  //   entry: {
+  //     homePage: `${admin_DIR}/index.jsx`
+  //   },
+  //   output: {
+  //     filename: 'bundle.js',
+  //     path: admin_DIST_DIR
+  //   },
+  //   module: {
+  //     rules: [{
+  //         test: /\.js[x]?/,
+  //         include: [admin_DIR],
+  //         loader: 'babel-loader',
+  //         query: {
+  //           presets: ['@babel/preset-env', '@babel/preset-react']
+  //         }
+  //       },
+  //       {
+  //         test: /\.css$/,
+  //         use: [{
+  //             loader: "style-loader" // creates style nodes from JS strings
+  //           },
+  //           {
+  //             loader: "css-loader" // translates CSS into CommonJS
+  //           }
+  //         ]
+  //       }
+  //     ]
+  //   },
+  //   resolve: {
+  //     extensions: ['.js', '.jsx']
+  //   }
+  // }
 ];

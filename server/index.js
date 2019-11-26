83d6168c6d16d5ce42e4bd21a83b239b414d7bc6
Adam Reback <adamreback@Adams-MBP.attlocal.net>
2019-11-26 11:54:07 -0800

    Server | Server now functional and ready to serve static files

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan  = require('morgan');
+const parser  = require('body-parser');
+const path    = require('path');
+const app     = express();
+const port    = process.env.port || 1000;
+
+app.use(morgan('dev'));
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
+
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(port, () => console.log(`Now listening on port ${port}`));

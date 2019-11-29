589319fe198758c644c8c6696ca7d25c068e3251
Adam Reback <adamreback@Adams-MBP.attlocal.net>
2019-11-28 17:53:24 -0800

    Database | CreateSub function now responds based on type of error Misc | Responses now stored in json based on error/response code

--- database/createSub.js
+++ database/createSub.js
@@ -1,32 +1,39 @@
 const db = require('./index.js');
+const resCodes = require('../responseCodes.json');
 
 
-function createSub (subName) {
-    let endpointName = subName.split(' ').join('-').toLowerCase();
+function createSub (subName, res) {
+    let endpointName = subName.split(' ').join('_').toLowerCase();
     db
         .query(`
             INSERT INTO subs (name, endpoint)
-            VALUES ('${subName}', '${endpointName}');
-            CREATE TABLE ${subName} (
-            id serial PRIMARY KEY
-            title VARCHAR(140)
-            upvotes INTEGER
-            downvotes INTEGER
-            linkURL VARCHAR(100)
+            VALUES ('${subName.toLowerCase()}', '${endpointName}');
+            CREATE TABLE IF NOT EXISTS ${endpointName} (
+            id serial PRIMARY KEY,
+            title VARCHAR(140),
+            upvotes INTEGER,
+            downvotes INTEGER,
+            linkURL VARCHAR(100),
             text VARCHAR(15000));
         `)
         .then(data => {
-            console.log(data)
             db.end();
+            if (res) {
+                res.send(resCodes["1"])
+            }
         })
         .catch(err => {
-            //TODO respond with error according to constraint violation
-            //note: check err.constaint to be 'subs_name_key' or 'subs_endpointname_key'
-            console.log(err.constraint);
             db.end();
+            if (res) {
+                if (err.constraint === 'subs_name_key') {
+                    resCodes["-1"];
+                } else {
+                    resCodes["-2"];
+                }
+            }
         })
 }
 
-createSub('doesnt works');
+createSub('Works once');
 
 module.exports = createSub;
